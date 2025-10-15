73527dc5eaeec8555d8afc2957e5ae07c697d6ec
3m1n3nc3 <mygames.ng@gmail.com>
2025-10-15 07:39:43 +0100

    update: add ecludes for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     include: ['tests/*.{test,spec}.?(c|m)[jt]s?(x)'],
     coverage: {
       reporter: ['text', 'json', 'html', 'lcov'],
+      exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*', '**/.h3ravel/**'],
     }
   }
 })
